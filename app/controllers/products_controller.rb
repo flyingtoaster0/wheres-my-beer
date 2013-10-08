c994af3f5c48c325837ce0d48290c749b0a3f4cd
flyingtoaster0 <Goombas5>
2013-10-07 22:16:06 -0400

    Started implementing text search for product names.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,16 +11,45 @@ class ProductsController < ApplicationController
 
     @stores = @inventory.collect do |i|
       store = Store.find(i.store_id)
-      {:store_id => store.id, :name => store.name, :city => store.city, :address_line_1 => store.address_line_1, :quantity => i.quantity}
+      {:name => store.name, :city => store.city, :address_line_1 => store.address_line_1, :quantity => i.quantity}
     end
-
 
   end
 
 
   def search
-    @query = params[:query]
-    # TODO do some intelligent-like full text search.
+    query = params[:query]
+
+    # Check to see if there is an exact product name match
+    @products = Product.where('lower(name) = ?', query.downcase!).collect{|p| p.id}
+
+    if @products.length == 1
+      # Found it. Redirect to product page here.
+    end
+
+    if @products.empty?
+      @products = Product.where('lower(name) like ?', query.slice!(/ .*/) + '%').collect{|p| p.id}
+
+      if @products.length == 1
+        # Found something. Redirect to product page here.
+      end
+
+      # Still nothing... tough. Try searching with just the first three letters and see what you can find.
+      if @products.empty?
+        @products = Product.where('lower(name) like ?', query[0..3] + '%').collect{|p| p.id}
+
+        if @products.length == 1
+          # Somehow found something. Redirect to product page here.
+        end
+      end
+    end
+
+    if @products.empty?
+      # If you STILL found nothing, take em back to the front page and show one of those nice "flash" errors.
+      # Be sure to pass params[:query] back to the front page so you can tell them what query didn't work.
+    end
+
+    # Otherwise, grats. You get to go to the view now.
   end
 
 end
