32d0c90560ffa971b7f361998714536c34716f89
flyingtoaster0 <Goombas5>
2013-10-13 18:23:30 -0400

    Time now shows properly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def msmTo12time(msm)
+    h24 = msm / 60;
+    mins = msm % 60;
+    h12  = (0 == h24 ? 12 : (h24 > 12 ? (h24 - 10) - 2 : h24))
+    ampm = (h24 >= 12 ? 'PM' : 'AM')
+
+    h12.to_s + ':' + sprintf("%02d",mins) + ' ' + ampm.to_s
+  end
 end
